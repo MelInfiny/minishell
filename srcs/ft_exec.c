d665ba344c24df84820045a69fe5d165f2433135
MelInfiny <Pb20ar.35@outlook.fr>
2022-12-09 02:03:20 +0100

    pipeline

--- srcs/ft_exec.c
+++ srcs/ft_exec.c
@@ -26,43 +26,63 @@ static int	execute(t_input *input, t_list *cmds)
 	return (1);
 }
 
-static void	create_pipe(t_input *input, t_list *cmds, int fd[2])
+static void	exec_cmd(t_input *input, t_list *cmds)
+{
+	int	pid;
+	int	status;
+	int	ret;
+
+	ret = -1;
+	pid = fork();
+	if (pid < 0)
+		return ;
+	if (pid == 0)				//fiston 
+		ret = execute(input, cmds);
+	else
+	{	
+		wait(&status);			// papa
+		exit(status);
+	}
+	exit(ret);
+}
+
+static int	create_pipe(t_input *input, t_list *cmds, int fd[2])
 {
 	int	pid;
 
 	pid = fork();
-	if (pid == 0)
+	if (pid == 0)			// papa
 	{
 		if (fd)
 		{
 			close(fd[0]);
 			print_break(input);
-			if (dup2(STDOUT_FILENO, fd[1]) == -1)
+			if (dup2(fd[1], STDOUT_FILENO) == -1)
 				return ;
 			print_break(input);
 			close(fd[1]);
 		}
-		execute(input, cmds);
+		exec_cmd(input, cmds);
 	}
-	else if (pid > 0)
+	else if (pid > 0)		// papy	
 	{
 		if (fd)
 		{
 			close(fd[1]);
 			print_break(input);
-			if (dup2(STDIN_FILENO, fd[0]) == -1)
+			if (dup2(fd[0], STDIN_FILENO) == -1)
 				return ;
 			print_break(input);
 			close(fd[0]);
 		}
-		waitpid(pid, NULL, 0);
-		//wait(&status);
 	}
+	return (pid);
 }
 
 void	ft_pipe(t_input *input)
 {
 	t_list	*tmp;
+	t_list	*pids;
 	int	count;
 	int	fd[2];
 
@@ -73,12 +93,31 @@ void	ft_pipe(t_input *input)
 		ft_cmd_error(input, NULL, "path");
 		return ;
 	}
-	while (count-- > 1)
+	while (count--)
 	{
+		// count = 0 dup stdin
+		// count = lst dup stdout
 		if (pipe(fd) < 0)
 			return ;
-		create_pipe(input, tmp, fd);
+		ft_lstadd(ft_lstnew(create_pipe(input, tmp, fd)));
 		tmp = tmp->next;
 	}
-	create_pipe(input, tmp, NULL);
+	count = ft_lstsize(tmp);
+	while (1)
+	{
+		while (pids)
+		{
+			ret = waitpid(pids->content, NULL, WNOHANG);
+			if (ret < 0)
+			{
+				perror("pid");
+				// kill
+				return ;
+			}
+			else if (ret > 0)
+				return ;	//kill
+			pids = pids->next;
+		}
+	}	
+	//create_pipe(input, tmp, NULL);
 }
