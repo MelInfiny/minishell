9b0d5a98e1d528f15ef50aec24e11bb640b9e7cb
MelInfiny <Pb20ar.35@outlook.fr>
2023-01-28 20:40:50 +0100

    tester minishell okay

--- test/monTester.sh
+++ test/monTester.sh
@@ -17,43 +17,44 @@ FILE="data/*_test"
 
 MS_OUT="trace/ms_res"
 BASH_OUT="trace/bash_res"
-RES="trace/res_"
-LEAKS="trace/leaks_"
+LEAKS="trace/leaks"
+RES="trace/res"
+EXIT="exit"
 compteur=1
 
 function repeat() {
 	for i in {1..50}; do echo -n "$1"; done
-	echo
+	echo -e "\n"
+}
+
+function lunch_shell() {
+	echo $@ | ../minishell > out 2>&1
+	sed '/^[\$]>/d;1,3d;' out > $MS_OUT	
+}
+
+function lunch_bash() {
+	echo $@ | bash > bout 2>&1
+	sed 's/bash: line [0-9][0-9]*[:] //g' bout > $BASH_OUT
 }
 
 function check_result() {
-	../minishell > out 2>&1
-	$1
-	sed '/^[\$]>/d;1,3d;' out > $MS_OUT
-	
-	exec 1>bash > bout 2>&1
-	$1
-	sed 's/bash: line [0-9][0-9]*[:] //g' bout > $BASH_OUT
-	echo "exit" >> $BASH_OUT
-	
-	local out=$(diff $BASH_OUT $MS_OUT | wc -l)
-	rm -f $LEAKS$compteur
 	repeat $stars
+	echo "$@"
+	local out=$(diff $BASH_OUT $MS_OUT | wc -l)
 	if [[ $out -eq 0 ]]; then
 		echo -en "${vert}${tiret}${tiret}RESULT OK : ${reset}"
 	else
 		echo -en "${rouge}${tiret}${tiret}RESULT FAIL : ${reset}"
-		echo $1 "\n" > $LEAKS$compteur
-		echo $out >> $LEAKS$compteur
-		repeat $stars >> $LEAKS$compteur
+		echo -e "$1\n" >> $LEAKS
+		echo -en ${rouge}$compteur:${reset} >> $LEAKS 
+		diff $BASH_OUT $MS_OUT >> $LEAKS
+		repeat $stars >> $LEAKS
 	fi
-#	rm -f out bout
+	echo $@ >> $RES && cat $MS_OUT >> $RES && repeat $stars >> $RES
 }
 
 function check_leaks() {
-	valgrind --leak-check=full  --show-leak-kinds=all --track-fds=yes ../minishell > leaks 2>&1
-	echo $1
-	echo "exit"
+	echo $@valgrind --leak-check=full  --show-leak-kinds=all --track-fds=yes ../minishell > leaks 2>&1
 	sed -r -n '/^(={2})([0-9]{4,})\1[ ]\LEAK SUMMARY:/,+3p' out | grep -E '[^0] bytes' >> $LEAKS$compteur
 	if [[ -s $LEAKS$compteur ]]; then
 		echo -e "${rouge}leaks found.${reset}"
@@ -65,13 +66,19 @@ function check_leaks() {
 }
 
 clear
+rm -f $LEAKS $BASH_OUT $MS_OUT $RES
+cd .. && make ; cd test
 
 echo "set enable-bracketed-paste off" > .inputrc
 export INPUTRC=$PWD/.inputrc
 
-while IFS= read -r line
-do
+while read -r line; do
+	lunch_shell $line
+	lunch_bash $line
 	check_result $line
-#	check_leaks $line
+	check_leaks $line
 	((compteur+=1))
+	rm -f out bout
 done < $FILE
+
+#cd .. && make fclean
