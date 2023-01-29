75c095334199f4b33aa21e3e0bb45f72784c013e
Emilie NOLBAS <enolbas@e3r12p20.clusters.42paris.fr>
2023-01-29 19:20:27 +0100

    new test cases for export unset

--- test/monTester.sh
+++ test/monTester.sh
@@ -13,10 +13,11 @@ stars="="
 tiret="-----"
 
 #FILES
-FILE="data/*_test"
+FILE="data/echo_test"
 
 MS_OUT="trace/ms_res"
 BASH_OUT="trace/bash_res"
+DIFF="trace/diff"
 LEAKS="trace/leaks"
 RES="trace/res"
 EXIT="exit"
@@ -28,46 +29,46 @@ function repeat() {
 }
 
 function lunch_shell() {
-	echo $@ | ../minishell > out 2>&1
+	echo -e $@ | ../minishell > out 2>&1
 	sed '/^[\$]>/d;1,3d;' out > $MS_OUT	
 }
 
 function lunch_bash() {
-	echo $@ | bash > bout 2>&1
+	echo -e $@ | bash > bout 2>&1
 	sed 's/bash: line [0-9][0-9]*[:] //g' bout > $BASH_OUT
 }
 
 function check_result() {
 	repeat $stars
-	echo "$@"
+	echo -e "$@"
 	local out=$(diff $BASH_OUT $MS_OUT | wc -l)
 	if [[ $out -eq 0 ]]; then
 		echo -en "${vert}${tiret}${tiret}RESULT OK : ${reset}"
 	else
 		echo -en "${rouge}${tiret}${tiret}RESULT FAIL : ${reset}"
-		echo -e "$1\n" >> $LEAKS
-		echo -en ${rouge}$compteur:${reset} >> $LEAKS 
-		diff $BASH_OUT $MS_OUT >> $LEAKS
-		repeat $stars >> $LEAKS
+		echo -e "$1\n" >> $DIFF
+		echo -en ${rouge}$compteur:${reset} >> $DIFF
+		diff $BASH_OUT $MS_OUT >> $DIFF
+		repeat $stars >> $DIFF
 	fi
 	echo $@ >> $RES && cat $MS_OUT >> $RES && repeat $stars >> $RES
 }
 
 function check_leaks() {
-	echo $@ | valgrind --leak-check=full  --show-leak-kinds=all --track-fds=yes ../minishell > leaks 2>&1
-	sed -r -n '/^(={2})([0-9]{4,})\1[ ]\LEAK SUMMARY:/,+3p' out | grep -E '[^0] bytes' >> $LEAKS$compteur
-	if [[ -s $LEAKS$compteur ]]; then
+	echo -e "$@" | valgrind --leak-check=full --show-leak-kinds=all --track-fds=yes --suppressions=../readline_leaks ../minishell > leaks 2>&1
+	sed -r -n '/^(={2})([0-9]{4,})\1[ ]\LEAK SUMMARY:/,+3p' leaks | grep -E '[^0] bytes' >> $LEAKS
+	if [[ -s $LEAKS ]]; then
 		echo -e "${rouge}leaks found.${reset}"
 	else
 		echo -e "${vert}no leaks.${reset}"
-	rm -f $LEAKS$compteur
+		rm -f $LEAKS
 	fi
-#rm -f leaks
+	rm -f leaks
 }
 
 clear
-rm -f $LEAKS $BASH_OUT $MS_OUT $RES
-cd .. && make ; cd test
+rm -f $LEAKS $DIFF $BASH_OUT $MS_OUT $RES
+make -C ../ >/dev/null
 
 echo "set enable-bracketed-paste off" > .inputrc
 export INPUTRC=$PWD/.inputrc
@@ -78,7 +79,7 @@ while read -r line; do
 	check_result $line
 	check_leaks $line
 	((compteur+=1))
-	rm -f out bout
+#	rm -f out bout
 done < $FILE
 
 #cd .. && make fclean
